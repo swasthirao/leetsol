60c97bd51e2757175ba1dd6078d01ee7d49013a8
swasthirao <swasthirao021@gmail.com>
2026-03-16 22:18:21 +0530

    Time: 0 ms (100.00%) | Memory: 24.5 MB (92.45%) - LeetSync

--- 2668-form-smallest-number-from-two-digit-arrays/form-smallest-number-from-two-digit-arrays.cpp
+++ 2668-form-smallest-number-from-two-digit-arrays/form-smallest-number-from-two-digit-arrays.cpp
@@ -6,8 +6,7 @@ int com = 10;
           for(int i=0;i<nums1.size();i++){
                   for(int j=0;j<nums2.size();j++){
                 if(nums1[i]==nums2[j]){
-                com=min(com,nums1[i]);
-                }
+                com=min(com,nums1[i]);                }
                
                   }
           }
