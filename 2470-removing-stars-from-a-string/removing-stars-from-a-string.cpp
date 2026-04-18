95638a29bbccc07b9f272c49e287c512daba80f7
swasthirao <swasthirao021@gmail.com>
2026-04-18 20:41:35 +0530

    Time: 11 ms (87.24%) | Memory: 28.5 MB (46.13%) - LeetSync

---
+++ 2470-removing-stars-from-a-string/removing-stars-from-a-string.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string removeStars(string s) {
+       string res="";
+
+    for(char ch : s){
+        if(ch == '*')
+        res.pop_back();
+        else
+        res.push_back(ch);
+    }
+       return res;
+    }
+};
